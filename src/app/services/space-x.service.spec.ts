7ba2498e8bdc51e8f29a469424b48b7e9b64d7b7
Riyaz <riyaz01234@gmail.com>
2021-01-15 22:41:14 +0530

    Fixed Unit test cases

--- src/app/services/space-x.service.spec.ts
+++ src/app/services/space-x.service.spec.ts
@@ -1,9 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { SpaceXService } from './space-x.service';
 
 describe('SpaceXService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports:[
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: SpaceXService = TestBed.get(SpaceXService);
